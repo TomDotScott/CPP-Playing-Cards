d8b58207016afe2a0c942ad643f44aba9b47eb3f
tomdotscott <tom.scott45456@gmail.com>
2020-01-22 15:21:05 +0000

    The rules
    Rules of the game are defined in a comment

---
+++ Cards/Cards/game.cpp
@@ -0,0 +1,17 @@
+#include "game.h"
+/*------------------------TODO---------------------------------------------
+ACE can be 1 or 11                                                           
+JACK, QUEEN and KING are 10 points                                           
+the CARDS are SHUFFLED                                                       
+each player is DEALT 2 CARDS                                                  
+the player is told the VALUE of their HAND                                     
+players decide whether to HIT or PASS                                              
+if HIT, the player has another CARD in their HAND                                  
+their total is told to them      
+players can be hit ONCE per round 
+if their new total is MORE THAN 21, they LOSE and are out of the game
+once the players have all chosen to HIT or PASS, the DECK is SHUFFLED again       
+when everyone has decided to PASS, their totals are worked out
+the player with the total closest to 21 wins
+
+-------------------------------------------------------------------------*/
